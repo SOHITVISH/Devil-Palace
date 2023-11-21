9a54aef6b94ff22bedda341ee9009d8da7087133
Sohit Vishwakarma <sohit777ma@gmail.com>
2023-11-21 15:47:10 +0530

    Devil

--- script/engine.js
+++ script/engine.js
@@ -1,7 +1,7 @@
 (function() {
   var Engine = window.Engine = {
 
-    SITE_URL: encodeURIComponent("http://adarkroom.doublespeakgames.com"),
+    SITE_URL: encodeURIComponent("/#"),
     VERSION: 1.3,
     MAX_STORE: 99999999999999,
     SAVE_DISPLAY: 30 * 1000,
